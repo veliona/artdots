38f0b1d96efd56e23388b5ae0da30a6cae452fa7
Julia Jakubczak <julia@julias-macbook-pro.home>
2024-07-28 16:57:40 +0200

    Add db and logo in nav bar

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,8 +1,30 @@
+import Image from 'next/image'
+
 export default function HomePage() {
     return (
-      <div>
-        <h1>Home Page</h1>
-        <p>Some content</p>
-      </div>
+        <nav>
+            <div className="header">
+                <Image
+                    src='logo.svg'
+                    width={100}
+                    height={100}
+                    alt="Logo Art Dots"
+                    />
+                <span className = "logoText">ARTDOTS</span>
+            </div>
+        </nav>
+        
+    //     <section id="art_type">
+    //     <div id="box">
+    //       <nav>
+    //         <ul>
+    //           <li><a href="#film">Film</a></li>
+    //           <li><a href="#music">Music</a></li>
+    //           <li><a href="#cv">CV</a></li>
+    //           <li><a href="#contact">CONTACT</a></li>
+    //         </ul>
+    //       </nav>
+    //   </div>
+    //   </section>
     );
   }
